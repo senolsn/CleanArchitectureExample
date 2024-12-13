d44be191563e2e0d22203a0d86e9d39f445bf282
Şenol Ş. <95684961+senolsn@users.noreply.github.com>
2024-12-13 21:09:25 +0300

    Infrastructure katmanı oluşturuldu.

---
+++ Infrastructure/ApplicationDbContext.cs
@@ -0,0 +1,23 @@
+﻿using Domain.Abstractions;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure
+{
+    public  class ApplicationDbContext : DbContext, IUnitOfWork
+    {
+        public ApplicationDbContext(DbContextOptions options)
+            : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(ApplicationDbContext).Assembly);
+        }
+    }
+}
